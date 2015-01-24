d03424657c7b88694b05e58f34b8041ee7ea8eed
Matt Neary <neary.matt@gmail.com>
2015-01-24 01:29:31 -0500

    tab bar view showing

--- js/app.js
+++ js/app.js
@@ -5,18 +5,27 @@
     this.templates = SlideMobileTemplates;
 
     this.initializeNavbarListeners();
-    this.presentTabBarView(this.$tabBar.find('.tab.active').data('target'));
+    this.contentViews = [].map.call(this.$tabBar.find('.tab'), function(tab) {
+      return $(tab).data('target');
+    }).map(function(target, i) {
+      var template = this.templates[target]();
+      var $template = $(template);
+      $template.attr("data-page-id", i+1);
+      this.$container.append($template);
+      return $template;
+    }.bind(this));
+    this.presentTabBarView(this.contentViews[0]);
   };
 
-  SlideMobile.prototype.presentTabBarView =  function (target) {
-    this.$container.html(this.templates[target]());
+  SlideMobile.prototype.presentTabBarView = function (target) {
+    $(".container").attr("data-page", target.data("page-id"));
   };
 
   SlideMobile.prototype.initializeNavbarListeners = function () {
     var self = this;
     this.$tabBar.find('.tab').on('click', function () {
       $(this).addClass('active').siblings().removeClass('active');
-      self.presentTabBarView($(this).data('target'));
+      self.presentTabBarView(self.contentViews[$(this).index()]);
     });
   };
 
