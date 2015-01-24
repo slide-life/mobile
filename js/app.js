fedcf3d60a17240dc46b8a25836126680ed9523a
Jack Dent <jack@jackdent.co.uk>
2015-01-24 14:11:08 -0500

    Add glyphicons and list views

--- js/app.js
+++ js/app.js
@@ -1,30 +1,126 @@
 (function ($) {
+  var REQUESTS = [
+    {
+      form: 'The Coop Signup',
+      fields: ['name', 'address', 'bank.card']
+    },
+    {
+      form: 'Insomnia Cookies Loyalty',
+      fields: ['name', 'email', 'mobile-phone']
+    }
+  ];
+
+  var CATEGORIES = {
+    banking: {
+      description: 'Banking',
+      fields: ['bank.account', 'bank.card']
+    },
+    'contact-details': {
+      description: 'Contact details',
+      fields: ['email', 'mobile-phone', 'home-phone', 'work-phone']
+    },
+    education: {
+      description: 'Education',
+      fields: ['university']
+    },
+    healthcare: {
+      description: 'Healthcare',
+      fields: ['allergies']
+    },
+    identification: {
+      description: 'Identification',
+      fields: ['signature', 'ssn', 'passport', 'id-card', 'drivers-license']
+    },
+    personal: {
+      description: 'Personal',
+      fields: ['name', 'profile-picture', 'date-of-birth', 'country-of-citizenship', 'gender', 'sex', 'occupation', 'place-of-birth', 'religion', 'ethnicity', 'spoken-languages', 'address']
+    }
+  };
+
   var SlideMobile = function () {
+    // Data
+    this.categories = CATEGORIES;
+    this.profile = {};
+    this.requests = REQUESTS;
+
+    // View logic
     this.$container = $('.container');
     this.$tabBar = $('.tab-bar');
     this.templates = SlideMobileTemplates;
+    this.pages = {};
 
+    // View initialization
     this.initializePages();
     this.initializeNavbarListeners();
-    this.presentPage(this.$tabBar.find('.tab.active').data('target'));
   };
 
   SlideMobile.prototype.initializePages = function () {
     var self = this;
-    this.$tabBar.find('.tab').map(function () {
-      var page = $(this).data('target');
-      var $template = $(self.templates[page]());
 
-      $template.attr('data-page', page);
+    this.pages.requests = this.initializeRequests();
+    this.pages.profile = this.initializeProfile();
+    this.pages.relationships = this.initializeRelationships();
 
-      self.$container.append($template);
+    $.each(this.pages, function (page, $html) {
+      self.$container.append($html);
     });
   };
 
-  SlideMobile.prototype.presentPage = function (target) {
-    this.$container.attr('data-page', target);
+  SlideMobile.prototype.initializeRequests = function () {
+    var self = this;
+    var $requests = this.buildPage('requests', { requests: this.requests });
+
+    $requests.on('click', '.list-item', function () {
+      var request = self.requests[$(this).data('target')];
+      Slide.Form.createFromIdentifiers($requests.find('.page.detail'), request.fields, function (form) {
+        form.build(self.profile, {
+          onSubmit: function () {
+            self.popToMaster();
+          }
+        });
+        self.pushToDetail();
+      });
+    });
+
+    return $requests;
+  };
+
+  SlideMobile.prototype.initializeProfile = function () {
+    var self = this;
+    var $profile = this.buildPage('profile', { categories: this.categories });
+
+    $profile.on('click', '.list-item', function () {
+      var category = self.categories[$(this).data('target')];
+      Slide.Form.createFromIdentifiers($profile.find('.page.detail'), category.fields, function (form) {
+        form.build(self.profile, {
+          onSubmit: function () {
+            self.popToMaster();
+          }
+        });
+        self.pushToDetail();
+      });
+    });
+
+    return $profile;
+  };
+
+  SlideMobile.prototype.initializeRelationships = function () {
+  };
+
+  SlideMobile.prototype.buildPage = function (page, data) {
+    var $template = $(this.templates[page](data));
+    $template.attr('data-page', page);
+    return $template;
+  };
+
+  SlideMobile.prototype.presentPage = function (page) {
+    this.$container.attr('data-page', page);
     this.$container.find('.content').removeClass('active');
-    this.$container.find('.content[data-page=' + target + ']').addClass('active');
+    this.$container.find('.content[data-page=' + page + ']').addClass('active');
+  };
+
+  SlideMobile.prototype.getPage = function (page) {
+    return this.pages[page];
   };
 
   SlideMobile.prototype.getActivePage = function () {
@@ -48,5 +144,6 @@
   };
 
   var app = new SlideMobile();
+  app.presentPage(app.$tabBar.find('.tab.active').data('target'));
 })($);
 
--- js/templates.js
+++ js/templates.js
@@ -1,8 +1,18 @@
 this["SlideMobileTemplates"] = this["SlideMobileTemplates"] || {};
 
-this["SlideMobileTemplates"]["profile"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<div class=\"content\">\n  <div class=\"master page\">\n    <h1>Profile</h1>\n  </div>\n  <div class=\"detail page\">\n    <h1>Detail</h1>\n  </div>\n</div>\n";
-  },"useData":true});
+this["SlideMobileTemplates"]["profile"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+  var helper, lambda=this.lambda, escapeExpression=this.escapeExpression, functionType="function", helperMissing=helpers.helperMissing;
+  return "        <li class=\"list-item\" data-target="
+    + escapeExpression(lambda((data && data.key), depth0))
+    + ">\n          <div class=\"title\">"
+    + escapeExpression(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"description","hash":{},"data":data}) : helper)))
+    + "</div>\n          <div class=\"annotation\"><span class=\"glyphicon glyphicon-menu-right\"></span></div>\n        </li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, buffer = "<div class=\"content\">\n  <div class=\"master page\">\n    <ul class=\"list-view\">\n";
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.categories : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { buffer += stack1; }
+  return buffer + "    </ul>\n  </div>\n  <div class=\"detail page\">\n    <h1>Detail</h1>\n  </div>\n  </div>\n</div>\n";
+},"useData":true});
 
 
 
@@ -12,6 +22,16 @@ this["SlideMobileTemplates"]["relationships"] = Handlebars.template({"compiler":
 
 
 
-this["SlideMobileTemplates"]["requests"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<div class=\"content\">\n  <div class=\"master page\">\n    <h1>Requests</h1>\n  </div>\n  <div class=\"detail page\">\n    <h1>Detail</h1>\n  </div>\n  </div>\n</div>\n";
-  },"useData":true});
+this["SlideMobileTemplates"]["requests"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+  var helper, lambda=this.lambda, escapeExpression=this.escapeExpression, functionType="function", helperMissing=helpers.helperMissing;
+  return "        <li class=\"list-item\" data-target="
+    + escapeExpression(lambda((data && data.index), depth0))
+    + ">\n          <div class=\"title\">"
+    + escapeExpression(((helper = (helper = helpers.form || (depth0 != null ? depth0.form : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"form","hash":{},"data":data}) : helper)))
+    + "</div>\n          <div class=\"annotation\"><span class=\"glyphicon glyphicon-menu-right\"></span></div>\n        </li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, buffer = "<div class=\"content\">\n  <div class=\"master page\">\n    <ul class=\"list-view\">\n";
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.requests : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { buffer += stack1; }
+  return buffer + "    </ul>\n  </div>\n  <div class=\"detail page\">\n    <h1>Detail</h1>\n  </div>\n  </div>\n</div>\n";
+},"useData":true});
